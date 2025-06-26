837cd8d1c4eb868d25aeace71c679c7dbc62fc62
Stanislaw Grin <stanislaw.grin@gmail.com>
2025-06-26 17:19:56 +0300

    Update src/modules/auth/ui/views/sign-in-view.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -147,7 +147,7 @@ export const SignInView = () => {
 
       <div
         className="text-muted-foreground &:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>
+        By clicking continue, you agree to our <a href="/terms">Terms of Service</a> and <a href="/privacy">Privacy Policy</a>
       </div>
     </div>
   )
