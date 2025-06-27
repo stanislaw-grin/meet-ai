f67ae087b0e809b9f7bc41aeadd7d87863d4229f
stanislaw.grin <stanislaw.grin@gmail.com>
2025-06-27 22:58:08 +0300

    Agents setup

--- src/modules/home/ui/views/home.tsx
+++ src/modules/home/ui/views/home.tsx
@@ -1,12 +1,5 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
-
-import { useTRPC } from '@/trpc/client'
-
 export const HomeView = () => {
-  const trps = useTRPC()
-  const { data } = useQuery(trps.hello.queryOptions({ text: 'Stanislaw' }))
-
-  return <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>
+  return <div>Home View</div>
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,19 +1,9 @@
-import { z } from 'zod'
+import { agentsRouter } from '@/modules/agents/server/procedures'
 
-import { baseProcedure, createTRPCRouter } from '../init'
+import { createTRPCRouter } from '../init'
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      }
-    }),
+  agents: agentsRouter,
 })
 
 // export type definition of API
