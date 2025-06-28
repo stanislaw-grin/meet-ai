64e7c7c0df4e72404295680b5c86b99b408c1b40
stanislaw.grin <stanislaw.grin@gmail.com>
2025-06-28 15:41:46 +0300

    Agents form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,22 +1,39 @@
 import { Suspense } from 'react'
 
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { ErrorBoundary } from 'react-error-boundary'
 
+import { auth } from '@/lib/auth'
+
+import { AgentsListHeader } from '@/modules/agents/ui/components/agents-list-header'
 import { AgentsView, AgentsViewError, AgentsViewLoading } from '@/modules/agents/ui/views/agents-view'
 import { getQueryClient, trpc } from '@/trpc/server'
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    redirect('/sign-in')
+  }
+
   const queryClient = getQueryClient()
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions())
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<AgentsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewError />}>
-          <AgentsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+    <>
+      <AgentsListHeader />
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<AgentsViewLoading />}>
+          <ErrorBoundary fallback={<AgentsViewError />}>
+            <AgentsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   )
 }
--- src/components/loading-state.tsx
+++ src/components/loading-state.tsx
@@ -5,7 +5,7 @@ interface Props {
   description: string
 }
 
-export const LoadingState: React.FC<Props> = ({ title, description }: Props) => {
+export const LoadingState = ({ title, description }: Props) => {
   return (
     <div className="py-4 px-8 flex flex-1 items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
