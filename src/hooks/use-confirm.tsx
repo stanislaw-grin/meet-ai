8fecdac1c9f4757d3199d66acca1b9125bb60193
stanislaw.grin <stanislaw.grin@gmail.com>
2025-06-29 18:13:49 +0300

    Agent Update & Delete

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -29,10 +29,10 @@ export const useConfirm = (title: string, description: string): [() => JSX.Eleme
   const ConfirmationDialog = () => (
     <ResponsiveDialog open={promise !== null} onOpenChange={handleClose} title={title} description={description}>
       <div className="pt-4 w-full flex flex-col-reverse gap-y-2 lg:flex-row gap-x-2 items-center justify-end">
-        <Button onClick={handleCancel} variant="outline" className="w-ull lg:w-auto">
+        <Button onClick={handleCancel} variant="outline" className="w-full lg:w-auto">
           Cancel
         </Button>
-        <Button onClick={handleConfirm} className="w-ull lg:w-auto">
+        <Button onClick={handleConfirm} className="w-full lg:w-auto">
           Confirm
         </Button>
       </div>
